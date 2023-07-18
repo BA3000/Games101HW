e6d738f72575e3eb67e058fd90e8c694384515e4
BA3000 <2864281096@qq.com>
2023-07-19 02:38:05 +0800

    update hw1, add model, projection matrices

---
+++ hw1/xmake.lua
@@ -0,0 +1,9 @@
+add_rules("mode.debug", "mode.release")
+add_requires("opencv", "eigen", {proxy=true})
+set_languages("cxx17")
+
+target("hw1")
+	set_kind("binary")
+	add_packages("opencv", "eigen")
+	add_files("src/*.cpp")
+
