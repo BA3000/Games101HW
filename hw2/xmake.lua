e8a4dab46948264da4012e0e2dce1a914f53eba2
BA3000 <2864281096@qq.com>
2024-12-11 01:25:04 +0800

    Update xmake.lua
    update target name

--- hw2/xmake.lua
+++ hw2/xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 add_requires("opencv", "eigen")
 set_languages("cxx17")
 
-target("hw0")
+target("hw2")
 	set_kind("binary")
 	add_packages("opencv", "eigen")
 	add_files("src/*.cpp")
